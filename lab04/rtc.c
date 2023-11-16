fa9f2f17188a8ea01db2310843ea463d8643df84
amlynczak <95677220+amlynczak@users.noreply.github.com>
2023-11-16 20:53:15 +0100

    lab04

---
+++ lab04/rtc.c
@@ -0,0 +1,49 @@
+#include "Driver_USART.h"
+#include "string.h"
+#include "LPC17xx.h"
+#include "PIN_LPC17xx.h"
+
+/* USART Driver */
+extern ARM_DRIVER_USART Driver_USART0;
+
+void RTC_IRQHandler(void){
+	const char* napis1 = "TIK";
+	const char* napis2 = "TAK";
+	const char* napis;
+	static bool flag = true;
+
+	if(flag){
+        napis = napis1;
+    }else{
+        napis = napis2;
+    }
+	
+	for(int i=0; i< sizeof(napis); i++){
+		if(LPC_UART0->LSR >> 6 == 1){ //sprawdzenie czy FIFO jest puste (dla 1 jest pusty i mozna przeslac kolejna litere)
+			LPC_UART0->THR = napis[i];
+		}else{
+			i--; //cofamy wskaznik na wczesniejszy i staramy sie wyslac w nastepnym wykonaniu petli for
+		}
+	}
+
+	LPC_RTC->ILR = 1;
+	flag = !flag;
+}
+
+int main(){
+    /*KONFIGURACJA USART*/
+    PIN_Configure(0, 2, 1, 2, 0);
+	PIN_Configure(0, 3, 1, 2, 0); 
+		
+	LPC_UART0->LCR = 3 | (1<<7); 
+	LPC_UART0->DLL = 27; 
+	LPC_UART0->DLM = 0; 
+	LPC_UART0->LCR = 3;
+    /*USART koniec - to samo*/
+
+    LPC_RTC->CCR = 1; //wlaczenie zegara RTC - Control Register
+	LPC_RTC->ILR = 1; //Interrupt Location Register - kontrola flagi przerwan RTC
+	LPC_RTC->CIIR = 1; //Counter Increment Interrupt Register - kontrola zdarzen RTC(co powoduje przerwanie itd.)
+
+    NVIC_EnableIRQ(RTC_IRQn); //wlaczenie przerwan dal RTC
+}
---
+++ lab04/timer0.c
@@ -0,0 +1,40 @@
+#include "Driver_USART.h"
+#include "string.h"
+#include "LPC17xx.h"
+#include "PIN_LPC17xx.h"
+
+/* USART Driver */
+extern ARM_DRIVER_USART Driver_USART0;
+
+void TIMER0_IRQHandler(void){
+	const char* napis = "ping\n";
+    LPC_TIM0->IR |= 1 << 0;
+	for(int i=0; i< sizeof(napis); i++){
+	    if(LPC_UART0->LSR >> 6 == 1){ //sprawdzenie czy FIFO jest puste (dla 1 jest pusty i mozna przeslac kolejna litere)
+			LPC_UART0->THR = napis[i];
+		}else{
+			i--; //cofamy wskaznik na wczesniejszy i staramy sie wyslac w nastepnym wykonaniu petli for
+		}
+	}
+}
+
+int main(){
+	/*KONFIGURACJA USART*/
+	PIN_Configure(0, 2, 1, 2, 0);
+	PIN_Configure(0, 3, 1, 2, 0); 
+		
+	LPC_UART0->LCR = 3 | (1<<7);
+	LPC_UART0->DLL = 27; 
+	LPC_UART0->DLM = 0; 
+	LPC_UART0->LCR = 3;
+    /*USART koniec - to samo co ostatnio*/
+	
+	LPC_TIM0->PR = 0; //ustawienie Prescaler Register - podzial czestotliwosci zegara wyjsciowego timera
+	LPC_TIM0->MR0 = 12500000; //Match Register 0 - gorna granica licznika timera
+	
+	LPC_TIM0->MCR = 0 | (1<<0) | (1<<1); //Match Cotrol Register - co ma sie dziac po wykryciu warunku dopasowania (dokumentacja)
+	
+	LPC_TIM0->TCR = 1; //Timer Control Register - ustawienie 1 na 1. bit (uruchomienie)
+	
+	NVIC_EnableIRQ(TIMER0_IRQn); // wlaczenie przerwan dla TIMER0 - z uzyciem TIMER0_IRQn
+}
